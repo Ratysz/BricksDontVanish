d4b1414ad92c345d2a1871421d8c60009c55e2f0
Ratys <ratysz@gmail.com>
2018-01-04 19:52:00 +0300

    Add project files.

---
+++ Source/Mod.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using System.Reflection;
+using Harmony;
+using Verse;
+using UnityEngine;
+
+namespace RTBricksDontVanish
+{
+	public class Mod : Verse.Mod
+	{
+		public Mod(ModContentPack content) : base(content)
+		{
+
+		}
+	}
+}
