accf0264aed1b69ef864064097625c1615b343db
Ratys <ratysz@gmail.com>
2018-01-06 04:49:39 +0300

    First round of patches are working.

--- Source/ModSettings.cs
+++ Source/ModSettings.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+using System.Reflection;
 using Verse;
+using RimWorld;
 using UnityEngine;
 
 namespace RTBricksDontVanish
@@ -11,13 +13,60 @@ namespace RTBricksDontVanish
 	public class ModSettings : Verse.ModSettings
 	{
 		// CONSTRUCTION:
-		// Failure notification on/off
-		// Fail chance % (of vanilla)
-		// Non-destructive failure chance % (of fail chance)
-		// Failure material loss % (of vanilla)
+		// - Failure notification on/off (on in vanilla)
+		// - Fail chance % (multiplier of vanilla)
+		// - Non-destructive failure chance % (of fail chance)
+		// + Failure material return % (50% in vanilla)
 
 		// DECONSTRUCTION:
-		// Material return % (of vanilla)
+		// + Deconstruction material return % (75% in vanilla)
+		// + True 100% on/off (off in vanilla)
+
+		private static int failureMaterialReturnPercentage = Mathf.RoundToInt((float)(
+			typeof(GenLeaving).GetField("LeaveFraction_FailConstruction", BindingFlags.NonPublic | BindingFlags.Static).GetValue(null)) * 100);
+		public static float FailureMaterialReturn
+		{
+			get
+			{
+				Log.Message("Get FailureMaterialReturn " + failureMaterialReturnPercentage / 100.0f);
+				return failureMaterialReturnPercentage / 100.0f;
+			}
+			set
+			{
+				failureMaterialReturnPercentage = Mathf.RoundToInt(FailureMaterialReturn * 100);
+			}
+		}
+
+		private static int deconstructionMaterialReturnPercentage = Mathf.RoundToInt(GenLeaving.LeaveFraction_DeconstructDefault * 100);
+		public static float DeconstructionMaterialReturn
+		{
+			get
+			{
+				Log.Message("Get DeconstructionMaterialReturn " + deconstructionMaterialReturnPercentage / 100.0f);
+				return deconstructionMaterialReturnPercentage / 100.0f;
+			}
+			set
+			{
+				deconstructionMaterialReturnPercentage = Mathf.RoundToInt(DeconstructionMaterialReturn * 100);
+			}
+		}
+
+		public static bool deconstructionTrue100 = false;
+
+		public override void ExposeData()
+		{
+			float failureMaterialReturn = FailureMaterialReturn;
+			float deconstructionMaterialReturn = DeconstructionMaterialReturn;
+			base.ExposeData();
+			Scribe_Values.Look(ref failureMaterialReturn, "failureMaterialReturn");
+			Scribe_Values.Look(ref deconstructionMaterialReturn, "deconstructionMaterialReturn");
+			Scribe_Values.Look(ref deconstructionTrue100, "deconstructionTrue100");
+			Log.Message("[BricksDontVanish]: settings initialized, failure material return is " + failureMaterialReturn
+				+ ", deconstruction material return is " + deconstructionMaterialReturn
+				+ " (" + (deconstructionTrue100 ? "true" : "sub 1") + ")");
+			failureMaterialReturnPercentage = Mathf.RoundToInt(failureMaterialReturn * 100);
+			deconstructionMaterialReturnPercentage = Mathf.RoundToInt(deconstructionMaterialReturn * 100);
+		}
 
 		public string SettingsCategory()
 		{
@@ -26,7 +75,47 @@ namespace RTBricksDontVanish
 
 		public void DoSettingsWindowContents(Rect rect)
 		{
-
+			Listing_Standard list = new Listing_Standard(GameFont.Small);
+			list.ColumnWidth = rect.width / 3;
+			list.Begin(rect);
+			list.Gap();
+			{
+				string buffer = failureMaterialReturnPercentage.ToString();
+				Rect rectLine = list.GetRect(Text.LineHeight);
+				Rect rectLeft = rectLine.LeftHalf().Rounded();
+				Rect rectRight = rectLine.RightHalf().Rounded();
+				Rect rectPercent = rectRight.RightPartPixels(Text.LineHeight);
+				rectRight = rectRight.LeftPartPixels(rectRight.width - Text.LineHeight);
+				Widgets.DrawHighlightIfMouseover(rectLine);
+				TooltipHandler.TipRegion(rectLine, "BricksDontVanish_FailureMaterialReturnTip".Translate());
+				TextAnchor anchorBuffer = Text.Anchor;
+				Text.Anchor = TextAnchor.MiddleLeft;
+				Widgets.Label(rectLeft, "BricksDontVanish_FailureMaterialReturnLabel".Translate());
+				Text.Anchor = anchorBuffer;
+				Widgets.TextFieldNumeric(rectRight, ref failureMaterialReturnPercentage, ref buffer, 0, 1000);
+				Widgets.Label(rectPercent, "%");
+			}
+			{
+				string buffer = deconstructionMaterialReturnPercentage.ToString();
+				Rect rectLine = list.GetRect(Text.LineHeight);
+				Rect rectLeft = rectLine.LeftHalf().Rounded();
+				Rect rectRight = rectLine.RightHalf().Rounded();
+				Rect rectPercent = rectRight.RightPartPixels(Text.LineHeight);
+				rectRight = rectRight.LeftPartPixels(rectRight.width - Text.LineHeight);
+				Widgets.DrawHighlightIfMouseover(rectLine);
+				TooltipHandler.TipRegion(rectLine, "BricksDontVanish_DeconstructionMaterialReturnTip".Translate());
+				TextAnchor anchorBuffer = Text.Anchor;
+				Text.Anchor = TextAnchor.MiddleLeft;
+				Widgets.Label(rectLeft, "BricksDontVanish_DeconstructionMaterialReturnLabel".Translate());
+				Text.Anchor = anchorBuffer;
+				Widgets.TextFieldNumeric(rectRight, ref deconstructionMaterialReturnPercentage, ref buffer, 0, 1000);
+				Widgets.Label(rectPercent, "%");
+			}
+			list.CheckboxLabeled(
+				"BricksDontVanish_DeconstructionTrue100Label".Translate(),
+				ref deconstructionTrue100,
+				"BricksDontVanish_DeconstructionTrue100Tip".Translate());
+			list.End();
 		}
 	}
 }
--- Source/Patches_GenLeaving.cs
+++ Source/Patches_GenLeaving.cs
@@ -8,9 +8,42 @@ using System.Reflection.Emit;
 using Harmony;
 using RimWorld;
 using Verse;
+using UnityEngine;
 
 namespace RTBricksDontVanish
 {
+	[HarmonyPatch]
+	static class Patch_GBRLC_FailConstruction
+	{
+		static MethodBase TargetMethod()
+		{
+			return typeof(GenLeaving).GetMethods(AccessTools.all).FirstOrDefault(method => method.Name.Contains("m__5"));
+		}
+
+		static void Postfix(ref int __result, int count)
+		{
+			__result = Math.Min(count, GenMath.RoundRandom(count * ModSettings.FailureMaterialReturn));
+		}
+
+		/*static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+		{
+			bool patched = false;
+			MethodInfo sneakyMethod = AccessTools.Property(typeof(ModSettings), nameof(ModSettings.FailureMaterialReturn)).GetGetMethod();
+			foreach (var instr in instructions.ToList())
+			{
+				if (!patched && instr.opcode == OpCodes.Ldc_R4)
+				{
+					patched = true;
+					yield return new CodeInstruction(OpCodes.Call, sneakyMethod);
+				}
+				else
+				{
+					yield return instr;
+				}
+			}
+		}*/
+	}
+
 	[HarmonyPatch]
 	static class Patch_GBRLC_Deconstruct
 	{
@@ -20,16 +53,52 @@ namespace RTBricksDontVanish
 				.FirstOrDefault(type => type.FullName.Contains("GetBuildingResourcesLeaveCalculator"))
 				.GetMethods(AccessTools.all).FirstOrDefault(method => method.Name.Contains("m__0"));
 		}
-	}
 
-	[HarmonyPatch]
-	static class Patch_GBRLC_FailConstruction
-	{
-		static MethodBase TargetMethod()
+		static void Postfix(ref int __result, int count)
 		{
-			return typeof(GenLeaving)/*.GetNestedTypes(AccessTools.all)
-				.FirstOrDefault(type => type.FullName.Contains("GetBuildingResourcesLeaveCalculator"))*/
-				.GetMethods(AccessTools.all).FirstOrDefault(method => method.Name.Contains("m__5"));
+			if (ModSettings.deconstructionTrue100)
+			{
+				__result = Math.Min(count, GenMath.RoundRandom(count * ModSettings.DeconstructionMaterialReturn));
+			}
+			else
+			{
+				__result = GenMath.RoundRandom(Mathf.Min(count - 1, count * ModSettings.DeconstructionMaterialReturn));
+			}
 		}
+
+		/*static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+		{
+			int patchState = 0;
+			MethodInfo sneakyMethod = AccessTools.Property(typeof(ModSettings), nameof(ModSettings.DeconstructionMaterialReturn)).GetGetMethod();
+			foreach (var instr in instructions.ToList())
+			{
+				switch (patchState)
+				{	// I'm almost sure inverting the state machine to have default as case 0 is more performant, but that's squeezing stones.
+					case 0:
+						if (instr.opcode == OpCodes.Ldarg_0)
+						{
+							patchState++;
+							yield return new CodeInstruction(OpCodes.Call, sneakyMethod);
+						}
+						else
+						{
+							yield return instr;
+						}
+						break;
+					case 1:
+						patchState++;
+						break;
+					case 2:
+						patchState++;
+						break;
+					case 3:
+						patchState++;
+						break;
+					default:
+						yield return instr;
+						break;
+				}
+			}
+		}*/
 	}
 }
