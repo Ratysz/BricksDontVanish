8f3ef93c7adb5f7b33e38705b748d48bcddef494
Alexander Sepity <ratysz@gmail.com>
2025-07-13 17:04:19 -0400

    Update to RimWorld 1.6.

---
+++ v1.6/Source/Patches_JobDriver_ConstructFinishFrame.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+using RimWorld;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using Verse;
+
+namespace RTBricksDontVanish
+{
+	[HarmonyPatch]
+	internal static class Patch_MakeNewToils
+	{
+		private static MethodBase TargetMethod()
+		{
+			return typeof(JobDriver_ConstructFinishFrame)
+				.GetNestedTypes(AccessTools.all).FirstOrDefault(name => name.Name.Contains("DisplayClass8"))
+				.GetMethods(AccessTools.all).FirstOrDefault(method => method.Name.Contains("b__1"));
+		}
+
+		private static IEnumerable<CodeInstruction> Transpiler(IEnumerable<CodeInstruction> instructions)
+		{
+			var markerMethod = AccessTools.Property(typeof(Rand), nameof(Rand.Value)).GetGetMethod();
+			var sneakyMethod = AccessTools.Method(typeof(Patch_MakeNewToils), nameof(Patch_MakeNewToils.PatchedRandValue));
+			return Transpilers.MethodReplacer(instructions, markerMethod, sneakyMethod);
+		}
+
+		private static float PatchedRandValue()
+		{
+			if (ModSettings.FailureChanceScaling != 0f)
+			{
+				return Rand.Value / ModSettings.FailureChanceScaling;
+			}
+			return 100f;
+		}
+	}
+}
